815e78eda8698ed7442632b711b5dd85b4c43db1
Hrishi2520 <hrishikeshshedge0@gmail.com>
2026-05-05 23:11:35 +0530

    stock challenge

---
+++ LeetCode/src/SellStock/StockChallenge.java
@@ -0,0 +1,21 @@
+class StockChallenge {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int currentProfit = 0;
+        int buy = prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < buy) {
+                buy = prices[i];
+            } else {
+                currentProfit = prices[i] - buy;
+                maxProfit = currentProfit > maxProfit ? currentProfit : maxProfit;
+            }
+        }
+        return maxProfit;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums = {1,2,8,4,9,5,7};
+		System.out.println("Max Profit: "+maxProfit(nums)));
+	}
+}
